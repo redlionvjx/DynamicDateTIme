4e6d77c2598213be6e119b4642b5767bd5927de6
jeff <redlionvjx@gmail.com>
2017-07-18 00:40:48 -0500

    Refactor SDC tests

--- DynamicDateTime/DynamicDateTime.Test/SDCUnitTest.cs
+++ DynamicDateTime/DynamicDateTime.Test/SDCUnitTest.cs
@@ -7,54 +7,53 @@ namespace DynamicDateTime.Test
     [TestClass]
     public class SDCUnitTest
     {
-        [TestCategory("UnitTest_Date_Factory")]
+        [TestCategory("SDC_Unit_Test")]
         [TestMethod]
         public void Current()
         {
             var now = DateTime.Now;
-
-            var actualDate1 = DateParser.Parse("current");
-            Assert.IsTrue(actualDate1.Date.Equals(now.Date), string.Format("expected value = {0}, actual = {1}", now, actualDate1));
+            var actualDate = DateParser.Parse("current");
+            Assert.AreEqual(now.Date, actualDate.Date);
         }
 
-        [TestCategory("UnitTest_Date_Factory")]
+        [TestCategory("SDC_Unit_Test")]
         [TestMethod]
         public void Future()
         {
             var now = DateTime.Today;
-            var expectDate = now.AddMonths(1);
+            var expectedDate = now.AddMonths(1);
             var actualDate = DateParser.Parse("future");
-            Assert.IsTrue(actualDate.Date.Equals(expectDate.Date), string.Format("expected value = {0}, actual = {1}", expectDate, actualDate));
+            Assert.AreEqual(expectedDate.Date, actualDate.Date);
         }
 
-        [TestCategory("UnitTest_Date_Factory")]
+        [TestCategory("SDC_Unit_Test")]
         [TestMethod]
         public void FutureDependent()
         {
-            var dependentDate = new DateTime(2016, 10, 28);
-            var expectDate = new DateTime(2016, 11, 28);
+            var dependentDate = DateTime.Today;
+            var expectedDate = dependentDate.AddMonths(1);
             var actualDate = DateParser.Parse("future", dependentDate);
-            Assert.IsTrue(actualDate.Date.Equals(expectDate.Date), string.Format("expected value = {0}, actual = {1}", expectDate, actualDate));
+            Assert.AreEqual(expectedDate.Date, actualDate.Date);
         }
 
-        [TestCategory("UnitTest_Date_Factory")]
+        [TestCategory("SDC_Unit_Test")]
         [TestMethod]
         public void Past()
         {
             var now = DateTime.Now;
-            var expectDate = now.AddMonths(-1);
-            var actualDate1 = DateParser.Parse("past");
-            Assert.IsTrue(actualDate1.Date.Equals(expectDate.Date), string.Format("expected value = {0}, actual = {1}", expectDate, actualDate1));
+            var expectedDate = now.AddMonths(-1);
+            var actualDate = DateParser.Parse("past");
+            Assert.AreEqual(expectedDate.Date, actualDate.Date);
         }
 
-        [TestCategory("UnitTest_Date_Factory")]
+        [TestCategory("SDC_Unit_Test")]
         [TestMethod]
         public void PastDependent()
         {
-            var dependentDate = new DateTime(2016, 10, 28);
-            var expectDate = new DateTime(2016, 9, 28);
+            var dependentDate = DateTime.Today;
+            var expectedDate = dependentDate.AddMonths(-1);
             var actualDate = DateParser.Parse("past", dependentDate);
-            Assert.IsTrue(actualDate.Date.Equals(expectDate.Date), string.Format("expected value = {0}, actual = {1}", expectDate, actualDate));
+            Assert.AreEqual(expectedDate.Date, actualDate.Date);
         }
     }
 }
